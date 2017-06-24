ec22d77100d0d7ea349ae7dee5eecfdbe2e35dab
fuenfp <fuenph@gmail.com>
2017-06-24 18:38:41 +0200

    Update influx2csv.py

--- influx2csv.py
+++ influx2csv.py
@@ -2,10 +2,10 @@ from influxdb import InfluxDBClient
 import csv
 import json
 
-query = "select mean(*) from power where time < now() - 1h group by time(1m) fill(none);"
+query = "select mean(*) from power where time < now() - 1m group by time(1m) fill(none);"
 client = InfluxDBClient('localhost', 8086, 'root', 'root', 'logging')
 result = client.query(query)
-rows = json.dumps(result)
+
 with open('test.csv', 'wb+') as f:
     dict_writer = csv.DictWriter(f, fieldnames=['time', 'l1', 'l2', 'l3'])
     dict_writer.writeheader()
