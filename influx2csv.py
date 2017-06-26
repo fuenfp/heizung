57d682ec2b0b3302edb89a8a82d5ec90d7b6447a
fuenfp <fuenph@gmail.com>
2017-06-26 23:06:42 +0200

    Update influx2csv.py

--- influx2csv.py
+++ influx2csv.py
@@ -10,7 +10,7 @@ result = client.query(query)
 with open('test.csv', 'wb+') as f:
     with open('query.csv', 'w') as f: 
         f.write('day;time;l1;l2;l3');
-        for i in result.get_points('power'):
+        for i in result.get_points('burner'):
             row = '\n' + str(i['time'])[0:10] + ';' + str(i['time'])[11:-1] + ';' + str(i['oil_dachs']) + ';' + str(i['dachs1']) + ';' +  str(i['dachs2']) + ';' + str(i['dachs3'])
             f.write(row) 
     
