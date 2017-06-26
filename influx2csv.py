a9b4f141b41621e08af7bbb7dfd8d317d746986b
fuenfp <fuenph@gmail.com>
2017-06-26 22:59:35 +0200

    Update influx2csv.py

--- influx2csv.py
+++ influx2csv.py
@@ -3,7 +3,7 @@ from influxdb import InfluxDBClient
 import csv
 import json
 
-query = "select mean(*) from burner where time < now() - 1m group by time(1m) fill(none);"
+query = "select * from burner where time < now() - 1m group by time(1m) fill(none);"
 client = InfluxDBClient('localhost', 8086, 'root', 'root', 'logging')
 result = client.query(query)
 
